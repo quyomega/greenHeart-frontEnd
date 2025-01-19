183a29b81624e87143643ff4d3d20a566485683a
quyomega <quyomega@gmail.com>
2025-01-19 23:05:44 +0700

    update: delete AdminPage

--- src/pages/AdminDashboard.js
+++ src/pages/AdminDashboard.js
@@ -1,25 +1,11 @@
 import React from "react";
-import ManagerMember from "../components/manangerMenber";
-import useAdminData from "../hooks/adminData";
-import CreateTask from "../components/creatTask"; 
-import "../assets/css/Admin.css"; // Liên kết tệp CSS đã được sửa đổi cho đẹp
-
 
 function AdminDashboard() {
-  const token = localStorage.getItem("token"); // Lấy token từ localStorage
-  const { allUsers } = useAdminData(token);
-
-
 
   return (
- 
     <div className="container">
       <h1>Admin Dashboard</h1>
       <p>Welcome to the admin panel. Manage your users and activities here.</p>
-      <ManagerMember token={token} allUsers={allUsers} />
-      <br/>
-      <CreateTask token={token} />
-      <br/>
       
     </div>
   );
